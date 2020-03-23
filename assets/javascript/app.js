931cd5d5dd45ef14d0dd277b53e22b8cce7d779f
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-03-22 20:26:45 -0700

    Created sub pages; changed css to be a ratio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,19 @@
 $(window).on("load", function() {
+  //Index Page
   setTimeout(function() {
     $(".fly-in-text").removeClass("hidden");
   }, 500);
+
+  //Other Pages
+  $(".logo-container").on({
+    click: function() {
+      window.location.href = "index.html";
+    },
+    mouseenter: function() {
+      $(".hover-fly-text").removeClass("hidden");
+    },
+    mouseleave: function() {
+      $(".hover-fly-text").addClass("hidden");
+    }
+  });
 });
