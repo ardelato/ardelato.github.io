4e9501398dde2168d06e7c80740fb0e8cbdc61a3
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-03-24 22:48:34 -0700

    Working on Portfolio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,4 +16,32 @@ $(window).on("load", function() {
       $(".hover-fly-text").addClass("hidden");
     }
   });
+
+  //Portfolio Page
+  $(".project").on({
+    mouseenter: function() {
+      $(this)
+        .find(".project-name")
+        .hide();
+      $(this)
+        .find(".project-description")
+        .show();
+      $(this)
+        .find(".project-image")
+        .show();
+      $(this).css("justify-content", "flex-start");
+    },
+    mouseleave: function() {
+      $(this)
+        .find(".project-name")
+        .show();
+      $(this)
+        .find(".project-description")
+        .hide();
+      $(this)
+        .find(".project-image")
+        .hide();
+      $(this).css("justify-content", "center");
+    }
+  });
 });
